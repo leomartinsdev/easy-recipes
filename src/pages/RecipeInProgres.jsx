883dadb3a71d1959649906a76ad401ee7aa1fee5
Leonardo Martins <leonardogustavo@outlook.com>
2023-06-12 19:43:52 -0300

    Requisito 38 - check

--- src/pages/RecipeInProgres.jsx
+++ src/pages/RecipeInProgres.jsx
@@ -12,6 +12,8 @@ function RecipeInProgres() {
   const [recipeInfo, setRecipeInfo] = useState({
     name: '',
     src: '',
+    category: '',
+    ingredients: [],
     instructions: '',
   });
 
@@ -19,9 +21,22 @@ function RecipeInProgres() {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const json = await response.json();
     const data = json.meals;
+    console.log(data); // é um array com um objeto com as chaves que são usadas no código abaixo.
+    const ingredientsArray = [];
+    const maxIngredients = 20;
+    for (let i = 1; i <= maxIngredients; i += 1) {
+      const ingredientKey = `strIngredient${i}`;
+      const ingredientValue = data[0][ingredientKey];
+
+      if (ingredientValue && ingredientKey !== '') {
+        ingredientsArray.push(ingredientValue);
+      }
+    }
     setRecipeInfo({
       name: data[0].strMeal,
       src: data[0].strImageSource,
+      category: data[0].strCategory,
+      ingredients: ingredientsArray,
       instructions: data[0].strInstructions,
     } || []);
   }
@@ -30,9 +45,21 @@ function RecipeInProgres() {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
     const json = await response.json();
     const data = json.drinks;
+    const ingredientsArray = [];
+    const maxIngredients = 20;
+    for (let i = 1; i <= maxIngredients; i += 1) {
+      const ingredientKey = `strIngredient${i}`;
+      const ingredientValue = data[0][ingredientKey];
+
+      if (ingredientValue && ingredientKey !== '') {
+        ingredientsArray.push(ingredientValue);
+      }
+    }
     setRecipeInfo({
       name: data[0].strMeal,
       src: data[0].strImageSource,
+      category: data[0].strCategory,
+      ingredients: ingredientsArray,
       instructions: data[0].strInstructions,
     } || []);
   }
@@ -59,10 +86,30 @@ function RecipeInProgres() {
       />
       <button data-testid="share-btn" type="button">Compartilhar</button>
       <button data-testid="favorite-btn" type="button">Favoritar</button>
-      <span data-testid="recipe-category">Categoria</span>
-      <div data-testid="instructions">
-        {recipeInfo.instructions}
+      <span data-testid="recipe-category">{recipeInfo.category}</span>
+      <br />
+      <div>
+        Ingredients:
+        <br />
+        {
+          recipeInfo.ingredients.map((ingredient, index) => (
+            <div key={ index }>
+              <label
+                htmlFor={ ingredient }
+                data-testid={ `${index}-ingredient-step` }
+              >
+                {ingredient}
+                {' '}
+                <input type="checkbox" name={ ingredient } id={ ingredient } />
+              </label>
+            </div>))
+        }
       </div>
+      <br />
+      <span data-testid="instructions">
+        {recipeInfo.instructions}
+      </span>
+      <br />
       <button data-testid="finish-recipe-btn" type="button">Finalizar Receita</button>
     </div>
   );
