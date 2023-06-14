cc4bedbb5c34032d9466a0534c38a9efb10ccc58
Leonardo Martins <leonardogustavo@outlook.com>
2023-06-13 22:28:02 -0300

    Requisito 41 feito

--- src/pages/RecipeInProgres.jsx
+++ src/pages/RecipeInProgres.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import EachIngredient from '../components/EachIngredient';
 import shareIcon from '../images/shareIcon.svg';
-import whiteHeatIcon from '../images/whiteHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import { saveItem, getItem } from '../services/localStorage';
 
 function RecipeInProgres() {
   const { id } = useParams();
-  console.log('id:', id);
   const history = useHistory();
   const locPath = history.location.pathname;
   const whatPage = locPath.includes('meals') ? '/meals' : '/drinks';
@@ -21,21 +22,63 @@ function RecipeInProgres() {
     setLinkCopied(true);
   }
 
+  const [isFavorite, setIsFavorite] = useState(false);
+
   // Very importante state. It saves all the current recipe's info.
   const [recipeInfo, setRecipeInfo] = useState({
-    name: '',
-    src: '',
+    id,
+    type: '',
+    nationality: '',
     category: '',
+    alcoholicOrNot: '',
+    name: '',
+    img: '',
     ingredients: [],
     instructions: '',
   });
 
-  // DA PRA REFATORAR E TRANSFORMAR EM UMA ASYNC SÓ USANDO A MESMA LÓGICA DO WHATPAGE QUE USEI NO SEARCHBAR
+  // Handles the favorite button click. Saves the recipe's information on the localStorage.
+  function handleFavoriteBtn() {
+    const shouldSave = {
+      id: recipeInfo.id,
+      type: recipeInfo.type,
+      nationality: recipeInfo.nationality,
+      category: recipeInfo.category,
+      alcoholicOrNot: recipeInfo.alcoholicOrNot,
+      name: recipeInfo.name,
+      image: recipeInfo.img,
+    };
 
+    const savedState = getItem('favoriteRecipes') || [];
+    let updatedState = [];
+    if (isFavorite) {
+      updatedState = savedState.filter((recipe) => recipe.id !== shouldSave.id);
+    } else {
+      updatedState = [...savedState, shouldSave];
+    }
+
+    saveItem('favoriteRecipes', updatedState);
+
+    setIsFavorite(!isFavorite);
+  }
+
+  function checkIfFavorite() {
+    const savedState = getItem('favoriteRecipes') || [];
+    savedState.map((recipe) => {
+      if (recipe.id === id) {
+        setIsFavorite(true);
+      }
+      return recipe;
+    });
+  }
+
+  // Handles the API request to get the recipe's information.
+  // Honestly, this would be better if the recipe's information from the RecipeDetails page was saved on the global context and i'd just use it in this page.
   async function getRecipe() {
     const url = whatPage === '/meals' ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}` : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
     const response = await fetch(url);
     const json = await response.json();
+    console.log('CHAMOU API');
     const data = whatPage === '/meals' ? json.meals : json.drinks; // é um array com um objeto com as chaves que são usadas no código abaixo.
     const ingredientsArray = [];
     const maxIngredients = 20;
@@ -47,73 +90,34 @@ function RecipeInProgres() {
         ingredientsArray.push(ingredientValue);
       }
     }
-    const nameStr = whatPage === '/meals' ? data[0].strMeal : data[0].strDrink;
+    const name = whatPage === '/meals' ? data[0].strMeal : data[0].strDrink;
+    const alcoholicOrNot = whatPage === '/meals' ? '' : data[0].strAlcoholic;
+    const nationality = whatPage === '/meals' ? data[0].strArea : '';
+    const img = whatPage === '/meals' ? data[0].strMealThumb : data[0].strDrinkThumb;
+    const type = whatPage === '/meals' ? 'meal' : 'drink';
+
     setRecipeInfo({
-      name: nameStr,
-      src: data[0].strImageSource,
+      id,
+      type,
+      nationality,
       category: data[0].strCategory,
+      alcoholicOrNot,
+      name,
+      img,
       ingredients: ingredientsArray,
       instructions: data[0].strInstructions,
-    } || []);
+    });
   }
 
-  // async function getFood() {
-  //   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-  //   const json = await response.json();
-  //   const data = json.meals; // é um array com um objeto com as chaves que são usadas no código abaixo.
-  //   const ingredientsArray = [];
-  //   const maxIngredients = 20;
-  //   for (let i = 1; i <= maxIngredients; i += 1) {
-  //     const ingredientKey = `strIngredient${i}`;
-  //     const ingredientValue = data[0][ingredientKey];
-
-  //     if (ingredientValue && ingredientKey !== '') {
-  //       ingredientsArray.push(ingredientValue);
-  //     }
-  //   }
-  //   setRecipeInfo({
-  //     name: data[0].strMeal, // Caso eu refatore para ficar só com 1 async, essa chave pode ser alterada para um ternário para receber strMeal ou strDrink baseado no whatPage
-  //     src: data[0].strImageSource,
-  //     category: data[0].strCategory,
-  //     ingredients: ingredientsArray,
-  //     instructions: data[0].strInstructions,
-  //   } || []);
-  // }
-
-  // async function getDrink() {
-  //   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-  //   const json = await response.json();
-  //   const data = json.drinks;
-  //   const ingredientsArray = [];
-  //   const maxIngredients = 20;
-  //   for (let i = 1; i <= maxIngredients; i += 1) {
-  //     const ingredientKey = `strIngredient${i}`;
-  //     const ingredientValue = data[0][ingredientKey];
-
-  //     if (ingredientValue && ingredientKey !== '') {
-  //       ingredientsArray.push(ingredientValue);
-  //     }
-  //   }
-  //   setRecipeInfo({
-  //     name: data[0].strDrink,
-  //     src: data[0].strImageSource,
-  //     category: data[0].strCategory,
-  //     ingredients: ingredientsArray,
-  //     instructions: data[0].strInstructions,
-  //   } || []);
-  // }
-
-  // const drinkOrFood = () => {
-  //   if (whatPage === '/meals') {
-  //     getFood();
-  //   } else {
-  //     getDrink();
-  //   }
-  // };
+  // This is just to check the recipeInfo received
+  useEffect(() => {
+    console.log(recipeInfo);
+  }, [recipeInfo]);
 
   // As soon as the component renders, check if we're dealing with meals or drinks.
   useEffect(() => {
     getRecipe();
+    checkIfFavorite();
   }, []);
 
   return (
@@ -121,8 +125,9 @@ function RecipeInProgres() {
       <h3 data-testid="recipe-title">{recipeInfo.name}</h3>
       <img
         data-testid="recipe-photo"
-        src={ recipeInfo.src }
+        src={ recipeInfo.img }
         alt="Foto da Comida"
+        style={ { width: 250 } }
       />
       <button data-testid="share-btn" type="button" onClick={ handleShareBtn }>
         {
@@ -130,8 +135,13 @@ function RecipeInProgres() {
         }
       </button>
       { /* Por algum motivo, a imagem do coração não aparece se eu deixa-la como src do btn */ }
-      <button src={ whiteHeatIcon } data-testid="favorite-btn" type="button">
-        {/* <img src={ whiteHeatIcon } alt="Favorite" /> */}
+      <button
+        src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+        data-testid="favorite-btn"
+        type="button"
+        onClick={ handleFavoriteBtn }
+      >
+        <img src={ isFavorite ? blackHeartIcon : whiteHeartIcon } alt="Favorite" />
       </button>
       <span data-testid="recipe-category">{recipeInfo.category}</span>
       <br />
