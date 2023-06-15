f79482b15a9e5f4cdf99a25298fffcff8625b20d
MauricioMartiniJunior <martinijunior.mauricio@gmail.com>
2023-06-14 21:29:07 -0300

    test

--- src/renderWithRouter.jsx
+++ src/renderWithRouter.jsx
@@ -3,11 +3,56 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
-  });
-};
+// const renderWithRouter = (component) => {
+//   const history = createMemoryHistory();
+//   return ({
+//     ...render(<Router history={ history }>{component}</Router>), history,
+//   });
+// };
 
-export default renderWithRouter;
+// export default renderWithRouter;
+
+function withRouter(component, history) {
+  return (
+    <Router history={ history }>
+      { component }
+    </Router>
+  );
+}
+
+export function renderWithRouter(
+  component,
+  {
+    initialEntries = ['/'],
+    history = createMemoryHistory({ initialEntries }),
+  } = {},
+) {
+  return {
+    ...render(withRouter(component, history)),
+    history,
+  };
+}
+
+export function renderWithRedux(component, options = {}) {
+  const {
+    initialState = {},
+    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
+  } = options;
+
+  return {
+    ...render(withRedux(component, store)),
+    store,
+  };
+}
+
+export function renderWithRouterAndRedux(component, options = {}) {
+  const {
+    initialEntries = ['/'],
+    history = createMemoryHistory({ initialEntries }),
+  } = options;
+
+  return {
+    ...renderWithRedux(withRouter(component, history), options),
+    history,
+  };
+}
